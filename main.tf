fb14d8fa574568fad72cb6efbf9ba8f3976ee566
chrisgreene <chrisgreene@gmail.com>
2021-08-11 19:35:26 -0600

    Testing

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 data "aws_region" "current" {
 }
 
+data "aws_region" "current" {}
+
 resource "random_string" "rand" {
-  length = 24
+  length  = 24
   special = false
-  upper = false
+  upper   = false
 }
 
 locals {
@@ -13,6 +15,7 @@ locals {
 
 resource "aws_resourcegroups_group" "resourcegroups_group" {
   name = "${local.namespace}-group"
+
   resource_query {
     query = <<-JSON
 {
@@ -37,35 +40,39 @@ resource "aws_kms_key" "kms_key" {
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${local.namespace}-state-bucket"
+  bucket        = "${local.namespace}-state-bucket"
   force_destroy = var.force_destroy_state
+
   versioning {
     enabled = true
   }
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "aws:kms"
+        sse_algorithm     = "aws:kms"
         kms_master_key_id = aws_kms_key.kms_key.arn
       }
     }
   }
+
   tags = {
     ResourceGroup = local.namespace
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket" {
-  bucket = "aws_s3_bucket.s3_bucket.id"
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
 resource "aws_dynamodb_table" "dynamodb_table" {
-  name = "${local.namespace}-state-lock"
-  hash_key = "LockID"
+  name         = "${local.namespace}-state-lock"
+  hash_key     = "LockID"
   billing_mode = "PAY_PER_REQUEST"
   attribute {
     name = "LockID"
